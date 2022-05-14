1c93dd6038d8a3313db6c8396c7e8dab912f50ad
vanhhoasen313 <ANH.DV0389@sinhvien.hoasen.edu.vn>
2022-05-14 10:42:54 +0700

    xong Transfer moneye

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const account1 = {
   owner: "Jonas Schmedtmann",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
+  pin: 1,
 };
 
 const account2 = {
@@ -38,65 +38,134 @@ let input_user = document.querySelector(".input_user");
 let input_pin = document.querySelector(".input_pin");
 let submitBtn = document.querySelector(".submitBtn");
 let left = document.querySelector(".left");
-// let type_movement = document.querySelector(".type_movement");
 let value_movement = document.querySelector(".value_movement");
-
 submitBtn.addEventListener("click", function () {
-  // Tạo element mới
-
-  //   let div = document.createElement("div");
-  //   div.innerHTML = `<div class="movement_row">
-  //   <div class="movement_row_inside">
-  //     <div class="type_movement">8 WITHDRAWAL</div>
-  //     <div class="date_movement">5/12/2022</div>
-  //     <div class="value_movement">$8,500.00</div>
-  //   </div>
-  // </div>`;
-  //   left.appendChild(div);
-
   for (const account of accounts) {
-    let arrIDs = [
+    let getID = [
       account.owner.slice(0, 1).toLowerCase(),
       account.owner
         .slice(account.owner.indexOf(" ") + 1, account.owner.indexOf(" ") + 2)
         .toLowerCase(),
     ];
     console.log();
-    let id = arrIDs.join("");
-    if (id == input_user.value && account.pin == input_pin.value) {
-      console.log("dang nhap thanh cong");
-      left.innerHTML = "";
+    let thisID = getID.join("");
+    if (thisID == input_user.value && account.pin == input_pin.value) {
+      console.log(account);
 
+      let total = 0;
+      let totalDepost = 0;
+      let totalWithdrawal = 0;
+      account.movements.map(function (x) {
+        total += x;
+        if (x < 0) {
+          totalWithdrawal += x;
+        } else {
+          totalDepost += x;
+        }
+      });
+      /* HIỂN THỊ TỔNG TIỀN ĐANG CÓ */
+      let balance_value = document.querySelector(".balance_value");
+      balance_value.innerText = `${total} $`;
+      /* HIỂN THỊ IN */
+      let bottom_lable_in = document.querySelector(".bottom-lable-in");
+      bottom_lable_in.innerText = `${totalDepost} $`;
+      /* HIỂN THỊ OUT */
+      let bottom_lable_out = document.querySelector(".bottom-lable-out");
+      bottom_lable_out.innerText = `${Math.abs(totalWithdrawal)} $`;
+      /* RESET */
+      left.innerHTML = "";
+      /* QUÉT MẢNG VÀ HIỂN THỊ GIAO DỊCH RA NGOÀI*/
       account.movements.forEach(function (mov, index) {
-        // let div = index;
-        // let type_movement = "ID" + index;
         let div = document.createElement("div");
-        // div = document.createElement("div");
-        console.log(index);
+
         if (mov < 0) {
           div.innerHTML = `<div class="movement_row">
-        <div class="movement_row_inside">
-        <div class="type_movement type_movement_withdrawal">${
-          index + 1
-        } WITHDRAWAL</div>
-        <div class="date_movement">5/12/2022</div>
-        <div class="value_movement">$ ${mov}</div>        
-         </div>
-       </div>`;
+            <div class="movement_row_inside">
+            <div class="type_movement type_movement_withdrawal">${
+              index + 1
+            } WITHDRAWAL</div>
+            <div class="date_movement">5/12/2022</div>
+            <div class="value_movement"> ${mov} $</div>        
+            </div>
+          </div>`;
         } else {
           div.innerHTML = `<div class="movement_row">
-          <div class="movement_row_inside">
-          <div class="type_movement type_movement_deposit">${
-            index + 1
-          } DEPOSIT</div>
-          <div class="date_movement">5/12/2022</div>
-          <div class="value_movement">$ ${mov}</div>        
-           </div>
-         </div>`;
+            <div class="movement_row_inside">
+            <div class="type_movement type_movement_deposit">${
+              index + 1
+            } DEPOSIT</div>
+            <div class="date_movement">5/12/2022</div>
+            <div class="value_movement"> ${mov} $</div>        
+            </div>
+          </div>`;
         }
         left.appendChild(div);
       });
 
+      let btn_money = document.querySelector(".btn_money");
+
+      btn_money.addEventListener("click", function () {
+        let input_transfer_to = document.querySelector(
+          ".input_transfer_to_money"
+        );
+        let input_amount_money = document.querySelector(".input_amount_money");
+        for (const anotherAccount of accounts) {
+          let getIdAnother = [
+            anotherAccount.owner.slice(0, 1).toLowerCase(),
+            anotherAccount.owner
+              .slice(
+                anotherAccount.owner.indexOf(" ") + 1,
+                anotherAccount.owner.indexOf(" ") + 2
+              )
+              .toLowerCase(),
+          ];
+          let anotherID = getIdAnother.join("");
+
+          if (
+            input_transfer_to.value == anotherID &&
+            input_transfer_to.value != thisID
+          ) {
+            anotherAccount.movements.push(Number(input_amount_money.value));
+            account.movements.push(Number(input_amount_money.value) * -1);
+            /* QUÉT MẢNG VÀ HIỂN THỊ GIAO DỊCH RA NGOÀI*/
+            left.innerHTML = "";
+            account.movements.forEach(function (mov, index) {
+              let div = document.createElement("div");
+
+              if (mov < 0) {
+                div.innerHTML = `<div class="movement_row">
+                  <div class="movement_row_inside">
+                  <div class="type_movement type_movement_withdrawal">${
+                    index + 1
+                  } WITHDRAWAL</div>
+                  <div class="date_movement">5/12/2022</div>
+                  <div class="value_movement"> ${mov} $</div>        
+                  </div>
+                </div>`;
+              } else {
+                div.innerHTML = `<div class="movement_row">
+                  <div class="movement_row_inside">
+                  <div class="type_movement type_movement_deposit">${
+                    index + 1
+                  } DEPOSIT</div>
+                  <div class="date_movement">5/12/2022</div>
+                  <div class="value_movement"> ${mov} $</div>        
+                  </div>
+                </div>`;
+              }
+              left.appendChild(div);
+            });
+            console.log(anotherAccount.movements);
+            console.log(account.movements);
+            // break;
+          } else {
+            // input_amount_money.value = "";
+            // input_transfer_to.value = "";
+            // break;
+          }
+          // console.log("chuyen tien thanh cong");
+        }
+      });
       break;
     }
   }
